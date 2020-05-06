bde474a60e5e123bdb0f64b0585491b2ab9e5ebb
trundn <trunghitachi@gmail.com>
2020-05-06 23:22:48 +1000

    Update script files used to run harvester processes.

--- script/harvest_instance3_two_cores.sh
+++ script/harvest_instance3_two_cores.sh
@@ -1,3 +1,3 @@
 # Launch twitter harvester process
 echo "Harvesting twitter data..."
-time mpiexec -n 2 python3 tweet_harvester.py -a authenConfig.json -f filterConfig.json
+time mpiexec -n 2 python3 tweet_harvester.py -a authenConfig.json -f filterConfig.json -d databaseConfig.json -m stream
