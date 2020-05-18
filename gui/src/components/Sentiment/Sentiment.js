85e41ac403a1d2bc00547e35d61e190a26c35bac
ndhngoc91 <ndhngoc91@gmail.com>
2020-05-19 05:07:37 +1000

    Updating Map V3.0

--- gui/src/components/Sentiment/Sentiment.js
+++ gui/src/components/Sentiment/Sentiment.js
@@ -1,15 +1,43 @@
 import React, {Component} from 'react'
 import {Map, TileLayer, GeoJSON} from 'react-leaflet'
-import statesData from '../../assets/poa2016.json'
 import axios from 'axios'
-import {Popup} from "leaflet";
+import './style.sass';
+import TreeMenu from 'react-simple-tree-menu';
+
+const treeData = [
+    {
+        key: 'australia',
+        label: 'Australia',
+        nodes: [
+            {
+                key: 'states-and-territories',
+                label: 'State and Territories',
+                nodes: [],
+                url: 'http://localhost:8000/tweets/polygon/2'
+            },
+            {
+                key: 'postal-areas-2016',
+                label: 'Postal Areas',
+                nodes: [],
+                url: 'http://localhost:8000/tweets/polygon/3'
+            }
+        ],
+        url: 'http://localhost:8000/tweets/polygon/1'
+    }
+];
+
 
 export default class Sentiment extends Component {
     state = {
         lat: -37.8136,
         lng: 144.9631,
-        zoom: 10,
-        data: 5
+        zoom: 8,
+        polygonData: null
+    }
+
+    constructor(props) {
+        super(props);
+        this.geoJsonLayer = React.createRef();
     }
 
     geoJSONStyle() {
@@ -22,15 +50,13 @@ export default class Sentiment extends Component {
     }
 
     onEachFeature(feature, layer) {
-        console.log(this.state)
         layer.on('click', () => {
             axios.post('http://127.0.0.1:8000/tweets/statistics-in-polygon/', feature.geometry.coordinates).then(response => {
-
                 if (response.status === 200) {
                     let data = response.data;
                     let popupContent = `<Popup>
                         <p>${feature.properties.feature_name}</p>
-                        <p>Number Of Positive Tweets: ${5}</p>
+                        <p>Number Of Positive Tweets: ${data.number_of_positive_tweets}</p>
                         <p>Number Of Neural Tweets: ${data.number_of_neural_tweets}</p>
                         <p>Number Of Negative Tweets: ${data.number_of_negative_tweets}</p>
                         </Popup>`;
@@ -60,20 +86,55 @@ export default class Sentiment extends Component {
         });
     }
 
+    componentDidMount() {
+        axios.get("http://localhost:8000/tweets/polygon/1/").then(response => {
+            if (response.status === 200) {
+                let polygonData = JSON.parse(response.data.content);
+                this.geoJsonLayer.current.leafletElement.clearLayers().addData(polygonData);
+                this.setState({
+                    polygonData: polygonData
+                });
+            }
+        });
+    }
+
+    onMenuItemClick(event) {
+        if (event.url !== null) {
+            axios.get(event.url).then(response => {
+                if (response.status === 200) {
+                    let polygonData = JSON.parse(response.data.content);
+                    this.geoJsonLayer.current.leafletElement.clearLayers().addData(polygonData);
+                    this.setState({
+                        polygonData: polygonData
+                    });
+                }
+            });
+        }
+
+    }
 
     render() {
         const position = [this.state.lat, this.state.lng]
         return (
-            <Map center={position} zoom={this.state.zoom}>
-                <TileLayer
-                    attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
-                <GeoJSON
-                    data={statesData}
-                    style={this.geoJSONStyle}
-                    onEachFeature={this.onEachFeature}
-                    onmouseover={this.onMouseOver}/>
-            </Map>
+            <div className={"relative"}>
+                <Map center={position} zoom={this.state.zoom}>
+                    <TileLayer
+                        attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+                    <GeoJSON
+                        key={"map"}
+                        data={this.state.polygonData}
+                        style={this.geoJSONStyle}
+                        onEachFeature={this.onEachFeature}
+                        onmouseover={this.onMouseOver}
+                        ref={this.geoJsonLayer}/>
+                </Map>
+                <div className={"fixed"}>
+                    <TreeMenu data={treeData} debounceTime={125} disableKeyboard={false}
+                              hasSearch onClickItem={event => this.onMenuItemClick(event)}
+                              resetOpenNodesOnDataUpdate={false}/>
+                </div>
+            </div>
         )
     }
 }
--- gui/src/index.js
+++ gui/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TabPanel from './TabPanel';
-
 import './index.css'
 
 ReactDOM.render(<TabPanel/>, document.querySelector('#root'));
