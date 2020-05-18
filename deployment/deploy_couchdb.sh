ac982baccd93cc3352e9006b9ba49fb1772f62de
Mayank Sharma <mayankshar21@yahoo.com.sg>
2020-05-19 07:59:04 +1000

    Setting up deployment for harvesters

--- deployment/deploy_couchdb.sh
+++ deployment/deploy_couchdb.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-. ./unimelb-comp90024-2020-grp-45-openrc.sh; ansible-playbook -vvv --ask-become-pass deploy_couchdb.yaml -i hosts.ini
+. ./unimelb-comp90024-2020-grp-45-openrc.sh; ansible-playbook --ask-become-pass deploy_couchdb.yaml -i hosts.ini
