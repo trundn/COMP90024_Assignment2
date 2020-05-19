06820b57a50a59179bc4c50a9bd707feb2c094a4
trundn <trunghitachi@gmail.com>
2020-05-19 20:18:26 +1000

    Ignore filter coordinate for politician in stream mode.

--- harvest/writer_job.py
+++ harvest/writer_job.py
@@ -25,10 +25,14 @@ class WriterJob(Runnable):
 
                 if (self.helper.is_match(full_text.lower(), self.config_loader.track)):
                     source, coordinates = self.helper.extract_coordinates(tweet)
+                    
+                    politician_type = self.config_loader.get_politician_type(tweet.user.screen_name)
+                    is_politician = (politician_type is not None) and (politician_type != "")
 
-                    if (coordinates or (self.ignore_coordinates_filter is True)):
+                    if (coordinates or (self.ignore_coordinates_filter is True) \
+                                    or is_politician):
                         inside_polygon = True
-                        if (self.ignore_coordinates_filter is False):
+                        if ((self.ignore_coordinates_filter is False) and (is_politician is False)):
                             inside_polygon = self.config_loader.within_geometry_filters(coordinates)
 
                         if (inside_polygon is True):
@@ -38,7 +42,6 @@ class WriterJob(Runnable):
                             converted_datetime = ""
                             if tweet.created_at != None:
                                 converted_datetime = tweet.created_at.strftime('%Y-%m-%d %H:%M:%S%z')
-                            politician_type = self.config_loader.get_politician_type(tweet.user.screen_name)
 
                             filter_data = {'_id' : tweet.id_str, 'created_at' : converted_datetime,\
                                         'text' : full_text, 'user' : tweet.user.screen_name, \
