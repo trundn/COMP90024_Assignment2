262e73087784cfcb5158c2de35efdc66c1bd8306
ndhngoc91 <ndhngoc91@gmail.com>
2020-05-19 22:27:22 +1000

    Updated maps

--- views/map-tracking/map.js
+++ views/map-tracking/map.js
@@ -1,5 +1,5 @@
 function (doc) {
-  if (doc.raw_data) {
+  if (doc.raw_data && doc.calculated_coordinates.length === 2) {
     var raw_data = doc.raw_data
     emit([raw_data.user.id, raw_data.user.screen_name], [doc.calculated_coordinates, doc.created_at], 1);
   }
