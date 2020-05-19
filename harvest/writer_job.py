4a03086909850be7aa37f066b84936d075e73a19
trundn <trunghitachi@gmail.com>
2020-05-19 20:26:51 +1000

    Ignore coordinate filter.

--- harvest/writer_job.py
+++ harvest/writer_job.py
@@ -25,33 +25,29 @@ class WriterJob(Runnable):
 
                 if (self.helper.is_match(full_text.lower(), self.config_loader.track)):
                     source, coordinates = self.helper.extract_coordinates(tweet)
-                    
                     politician_type = self.config_loader.get_politician_type(tweet.user.screen_name)
-                    is_politician = (politician_type is not None) and (politician_type != "")
+                    
+                    inside_polygon = True
+                    if (coordinates):
+                        inside_polygon = self.config_loader.within_geometry_filters(coordinates)
 
-                    if (coordinates or (self.ignore_coordinates_filter is True) \
-                                    or is_politician):
-                        inside_polygon = True
-                        if ((self.ignore_coordinates_filter is False) and (is_politician is False)):
-                            inside_polygon = self.config_loader.within_geometry_filters(coordinates)
+                    if (inside_polygon is True):
+                        emotions = self.helper.extract_emotions(full_text)
+                        word_count, pronoun_count = self.helper.extract_word_count(full_text)
 
-                        if (inside_polygon is True):
-                            emotions = self.helper.extract_emotions(full_text)
-                            word_count, pronoun_count = self.helper.extract_word_count(full_text)
+                        converted_datetime = ""
+                        if tweet.created_at != None:
+                            converted_datetime = tweet.created_at.strftime('%Y-%m-%d %H:%M:%S%z')
 
-                            converted_datetime = ""
-                            if tweet.created_at != None:
-                                converted_datetime = tweet.created_at.strftime('%Y-%m-%d %H:%M:%S%z')
-
-                            filter_data = {'_id' : tweet.id_str, 'created_at' : converted_datetime,\
+                        filter_data = {'_id' : tweet.id_str, 'created_at' : converted_datetime,\
                                         'text' : full_text, 'user' : tweet.user.screen_name, \
                                         'politician' : politician_type, 'calculated_coordinates' : coordinates, \
                                         'coordinates_source' : source, 'emotions': emotions, \
                                         'tweet_wordcount' : word_count, "pronoun_count" : pronoun_count,\
                                         'raw_data' : tweet._json}
                             
-                            print(f"{tweet.id_str}    {emotions}    {word_count}    {pronoun_count}")
-                            self.db_connection.write_tweet(filter_data)
+                        print(f"{tweet.id_str}    {emotions}    {word_count}    {pronoun_count}")
+                        self.db_connection.write_tweet(filter_data)
             except:
                 print("Exception", sys.exc_info()[0], "occurred.")
                 traceback.print_exc(file = sys.stdout)
