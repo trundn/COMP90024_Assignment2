d77b483125cd4aa875e8bd70ab9f736c3fc84f6e
trundn <trunghitachi@gmail.com>
2020-05-20 09:03:55 +1000

    Just run tweeetid mode when having harvested dataset.

--- harvest/tweepy_tweetid_query.py
+++ harvest/tweepy_tweetid_query.py
@@ -31,34 +31,35 @@ class TweetIdQueryThread(threading.Thread):
         processor_size = comm.Get_size()
 
         dataset_folders = self.config_loader.get_tweeid_dataset(rank, processor_size)
-
-        # Get exectuor configuration to initialise thread pool job exectuor
-        num_thread, thread_names = self.config_loader.get_tweetid_executors_config(rank)
-        self.threadpool_job_executor = JobExecutor(num_thread, num_thread, 5000)
         
-        # Update thread name
-        for index in range(num_thread):
-            self.threadpool_job_executor.set_thread_name(index, thread_names[index])
+        if (dataset_folders is not None and len(dataset_folders) > 0):
+            # Get exectuor configuration to initialise thread pool job exectuor
+            num_thread, thread_names = self.config_loader.get_tweetid_executors_config(rank)
+            self.threadpool_job_executor = JobExecutor(num_thread, num_thread, 5000)
+            
+            # Update thread name
+            for index in range(num_thread):
+                self.threadpool_job_executor.set_thread_name(index, thread_names[index])
 
-        # Get tweeter authentication keys for tweetid repo harvest mode
-        authen_info = self.config_loader.get_tweetid_authentications(rank, num_thread)
-        
-        # Instantiate tweepy apis
-        tweepy_apis = {}
-        api_factory = APIFactory()
-        for key, value in authen_info.items():
-            tweepy_api = api_factory.create_api(value.api_key, value.api_secret_key,
-                                value.access_token, value.access_token_secret)
-            tweepy_apis[key] = tweepy_api
+            # Get tweeter authentication keys for tweetid repo harvest mode
+            authen_info = self.config_loader.get_tweetid_authentications(rank, num_thread)
+            
+            # Instantiate tweepy apis
+            tweepy_apis = {}
+            api_factory = APIFactory()
+            for key, value in authen_info.items():
+                tweepy_api = api_factory.create_api(value.api_key, value.api_secret_key,
+                                    value.access_token, value.access_token_secret)
+                tweepy_apis[key] = tweepy_api
 
-        for folder in dataset_folders:
-            print(f"Processing tweetid data set folder [{folder}]")
-            job = TweetIdProcessJob(tweepy_apis, self.config_loader, self.writer, folder)
-            self.threadpool_job_executor.queue(job)
+            for folder in dataset_folders:
+                print(f"Processing tweetid data set folder [{folder}]")
+                job = TweetIdProcessJob(tweepy_apis, self.config_loader, self.writer, folder)
+                self.threadpool_job_executor.queue(job)
 
-        while(self.threadpool_job_executor.is_any_thread_alive() is True):
-             # Sleep 1 second before checking thread alive status again
-            time.sleep(constants.ONE_SECOND)
+            while(self.threadpool_job_executor.is_any_thread_alive() is True):
+                # Sleep 1 second before checking thread alive status again
+                time.sleep(constants.ONE_SECOND)
 
-        print("Finished harvesting from tweetid datasets.")
+            print("Finished harvesting from tweetid datasets.")
 
