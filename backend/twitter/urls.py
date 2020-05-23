69cad37d71c7fd943331d59382dbc7a81a0599d8
ndhngoc91 <ndhngoc91@gmail.com>
2020-05-23 18:50:31 +1000

    Refactoring

--- backend/twitter/urls.py
+++ backend/twitter/urls.py
@@ -11,19 +11,31 @@ twitter_router = DefaultRouter()
 twitter_router.register('', TwitterViewSet, basename='twitter')
 
 urlpatterns = [
+    # urls for Home
+    path('tweets-by-categories/', TweetsByCategoriesView.as_view()),
+    path('tweets-with-coordinates/', TweetsWithCoordinatesView.as_view()),
+
+    # sentiment
     path('polygon/', include(polygon_router.urls)),
-    path('tweets-per-hour/', TweetsPerHourView.as_view()),
-    path('total-tweets-by-day-and-hour/', TotalTweetsByDayAndHour.as_view()),
-    path('language-statistics/', LanguageStatisticsView.as_view()),
-    path('tweets-with-emo-values-and-pro-cnt/', TweetsWithEmoValuesAndProCnt.as_view()),
     path('tweets-in-rectangle/', TweetsInRectangleView.as_view()),
     path('tweets-in-polygon/', TweetsInPolygonView.as_view()),
     path('statistics-in-polygon/<pk>/', StatisticsInPolygonView.as_view()),
+
+    # sentiment analysis
+    path('tweets-with-emo-values-and-pro-cnt/', TweetsWithEmoValuesAndProCnt.as_view()),
+
+    # movement
     path('movement/', MovementView.as_view()),
+
+    # user tracker
     path('find-route/', FindRouteView.as_view()),
     path('get-most-active-users/', GetMostActiveUsersView.as_view()),
     path('get-user-info/<pk>', GetUserInfoView.as_view()),
-    path('tweets-by-categories/', TweetsByCategoriesView.as_view()),
-    path('tweets-with-coordinates/', TweetsWithCoordinatesView.as_view()),
+
+    # statistics
+    path('tweets-per-hour/', TweetsPerHourView.as_view()),
+    path('language-statistics/', LanguageStatisticsView.as_view()),
+    path('total-tweets-by-day-and-hour/', TotalTweetsByDayAndHour.as_view()),
+
     path('document/', include(twitter_router.urls))
 ]
